0424a7df9e9a39d41c1f23d0cdabab7af7a349e4
DiegoxK <diego.stv@outlook.es>
2024-06-03 19:24:12 -0500

    Added setDone mutation to todoRouter

--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -1,7 +1,25 @@
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { todos } from "@/server/db/schema";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
 
 export const todoRouter = createTRPCRouter({
-  getTodos: publicProcedure.query(async () => {
-    return [10, 20, 30];
+  getTodos: publicProcedure.query(({ ctx }) => {
+    return ctx.db.select().from(todos);
   }),
+  setDone: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+        done: z.boolean(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db
+        .update(todos)
+        .set({
+          done: input.done,
+        })
+        .where(eq(todos.id, input.id));
+    }),
 });
