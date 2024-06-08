b23f924fa54022ba0f0cf76566a032cbc0393708
DiegoxK <diego.stv@outlook.es>
2024-06-07 19:55:38 -0500

    Updated EMAIL_SERVER_PORT to be a number in env.js

--- src/env.js
+++ src/env.js
@@ -26,7 +26,7 @@ export const env = createEnv({
     DISCORD_CLIENT_SECRET: z.string(),
 
     EMAIL_SERVER_HOST: z.string(),
-    EMAIL_SERVER_PORT: z.string(),
+    EMAIL_SERVER_PORT: z.coerce.number(),
     EMAIL_SERVER_USER: z.string(),
     EMAIL_SERVER_PASSWORD: z.string(),
     EMAIL_FROM: z.string().email(),
