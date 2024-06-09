5191cb415da90b6fe33cba2d29de2b7e3eb4802c
DiegoxK <diego.stv@outlook.es>
2024-06-09 17:45:00 -0500

    chore: Update sign-up page layout and styling

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,3 +1,16 @@
+import { Link } from "lucide-react";
+import SignUpForm from "./_components/signup-form";
+
 export default function SignUpPage() {
-  return <div>Register</div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="flex max-w-[600px] flex-col items-center gap-5">
+        <Link size={80} className="text-primary" />
+        <h1 className="bg-gradient-to-r from-primary to-secondary bg-clip-text text-4xl font-bold text-transparent">
+          Sign Up!
+        </h1>
+        <SignUpForm />
+      </div>
+    </main>
+  );
 }
