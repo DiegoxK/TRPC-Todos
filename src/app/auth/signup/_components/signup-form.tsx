e3860f52ec327039679a3ef8dfcee7b5ca87c01a
DiegoxK <diego.stv@outlook.es>
2024-06-09 17:26:46 -0500

    Added signup directories

---
+++ src/app/auth/signup/_components/signup-form.tsx
@@ -0,0 +1,95 @@
+"use client";
+import { signIn } from "next-auth/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Discord } from "@/components/vectors";
+import { getBaseUrl } from "@/trpc/react";
+
+import { useState } from "react";
+
+const formSchema = z.object({
+  email: z.string().email(),
+});
+
+export default function SignUpForm() {
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setLoading(true);
+    await signIn("email", { email: values.email });
+  }
+
+  // TODO: Add error handling
+  //TODO: Create Register page
+  // TODO: If the user is logged in, redirect them to the home page
+
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-3"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Sign in with your email address</FormLabel>
+                <FormControl>
+                  <Input placeholder="example@todos.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            disabled={loading}
+            className="w-full bg-gradient-to-br from-primary to-secondary"
+            type="submit"
+          >
+            {loading ? <div className="loader"></div> : <span>Submit</span>}
+          </Button>
+        </form>
+      </Form>
+      <div className="flex w-full items-center justify-center gap-4">
+        <Separator className="w-[115px] bg-white" />
+        <p>OR</p>
+        <Separator className="w-[115px] bg-white" />
+      </div>
+      <Button
+        disabled={loading}
+        className="flex w-full items-center justify-between px-5 font-medium transition-colors hover:bg-secondary"
+        size="lg"
+        onClick={() =>
+          signIn("discord", {
+            callbackUrl: getBaseUrl(),
+          })
+        }
+      >
+        Sign in with Discord
+        <Discord className="fill-foreground" width={33} height={33} />
+      </Button>
+    </>
+  );
+}
