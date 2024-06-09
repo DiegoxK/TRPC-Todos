8732f91ce4aacb2c3bc5d33ee8f720d883d4715c
DiegoxK <diego.stv@outlook.es>
2024-06-09 18:41:52 -0500

    feat: Implement setup account page for user to set username and profile picture

---
+++ src/app/setup/page.tsx
@@ -0,0 +1,6 @@
+export default function SetupAccount() {
+  // TODO: Implement setup account page
+
+  // This page should allow the user to setup their username and profile picture if they don't have one
+  return <div>SetupAccount</div>;
+}
