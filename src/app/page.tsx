cc4c913936a5a22e5b7d7209807719b18d8cb9bf
DiegoxK <diego.stv@outlook.es>
2024-06-08 20:25:14 -0500

    Added todo and show user email if loged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,23 @@ import Link from "next/link";
 export default async function Home() {
   const session = await getServerAuthSession();
 
+  // TODO: if there is a session, remove all verification codes of this user from db
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="from-primary to-secondary bg-gradient-to-r bg-clip-text text-4xl font-bold text-transparent">
+      <h1 className="bg-gradient-to-r from-primary to-secondary bg-clip-text text-4xl font-bold text-transparent">
         Todo App
       </h1>
       <p className="text-center text-lg">The best todo app in the world!</p>
+      {session ? session.user.email : null}
       {session ? <SignOut /> : null}
       <p>
         To get started,{" "}
-        <Link className="text-primary hover:text-secondary" href="/auth/signin">
+        <Link
+          replace
+          className="text-primary hover:text-secondary"
+          href="/auth/signin"
+        >
           please sign in
         </Link>
         .
