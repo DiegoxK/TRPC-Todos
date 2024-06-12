f271969632116cac24d839b9131f41c9a4fae3ab
DiegoxK <diego.stv@outlook.es>
2024-06-12 08:08:09 -0500

    Add setup form to SetupAccount page

--- src/app/(routes)/setup/page.tsx
+++ src/app/(routes)/setup/page.tsx
@@ -1,6 +1,12 @@
+import SetupForm from "./_components/setup-form";
+
 export default function SetupAccount() {
   // TODO: Implement setup account page
 
   // This page should allow the user to setup their username and profile picture if they don't have one
-  return <div>SetupAccount</div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <SetupForm />
+    </main>
+  );
 }
