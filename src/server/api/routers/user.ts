741f7bbb8ea64f5fc193c18614240a789a77eea0
DiegoxK <diego.stv@outlook.es>
2024-06-12 13:38:39 -0500

    Remove unused code in user router

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -8,30 +8,6 @@ import { eq } from "drizzle-orm";
 import { z } from "zod";
 
 export const userRouter = createTRPCRouter({
-  hasUserName: protectedProcedure
-    .input(
-      z.object({
-        email: z.string().email(),
-      }),
-    )
-    .query(async ({ ctx, input }) => {
-      const userEmail = input.email;
-
-      const user = await ctx.db.query.users.findFirst({
-        where: (table, funcs) => funcs.eq(table.email, userEmail),
-      });
-
-      if (!user) {
-        throw new Error("User not found");
-      }
-
-      if (!user.name) {
-        return false;
-      }
-
-      return true;
-    }),
-
   createUser: publicProcedure
     .input(
       z.object({
