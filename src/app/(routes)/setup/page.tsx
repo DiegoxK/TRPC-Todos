99aaa6f8385f4709463c2fe2a1096ffd157e0038
DiegoxK <diego.stv@outlook.es>
2024-06-12 13:28:07 -0500

    Add server authentication check to setup account page

--- src/app/(routes)/setup/page.tsx
+++ src/app/(routes)/setup/page.tsx
@@ -1,12 +1,20 @@
+import { getServerAuthSession } from "@/server/auth";
+import { redirect } from "next/navigation";
 import SetupForm from "./_components/setup-form";
 
-export default function SetupAccount() {
+export default async function SetupAccount() {
+  const session = await getServerAuthSession();
   // TODO: Implement setup account page
+
+  if (!session) {
+    // If the user is not authenticated, redirect them to the sign in page
+    return redirect("/auth/signin");
+  }
 
   // This page should allow the user to setup their username and profile picture if they don't have one
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <SetupForm />
+      <SetupForm email={session.user.email} />
     </main>
   );
 }
