f84a385a4616cee91f00f077d7c784952973753a
DiegoxK <diego.stv@outlook.es>
2024-06-12 13:37:41 -0500

    Update Dashboard page to include user profile picture

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,3 +1,30 @@
-export default function Dashboard() {
-  return <div>Dashboard</div>;
+import { getServerAuthSession } from "@/server/auth";
+import { redirect } from "next/navigation";
+import Image from "next/image";
+
+export default async function Dashboard() {
+  const session = await getServerAuthSession();
+
+  if (session) {
+    const hasUserName = session.user.name;
+
+    if (!hasUserName) {
+      redirect("/setup");
+    }
+  }
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome, {session?.user.name}</p>
+      {session?.user.image && (
+        <Image
+          src={session.user.image}
+          alt="profile picture"
+          width={200}
+          height={200}
+        />
+      )}
+    </div>
+  );
 }
