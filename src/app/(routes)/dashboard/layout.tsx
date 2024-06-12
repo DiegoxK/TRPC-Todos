5140160dec04183ae282630729ff6c57afa39583
DiegoxK <diego.stv@outlook.es>
2024-06-12 18:05:35 -0500

    Add Dashboard layout component

---
+++ src/app/(routes)/dashboard/layout.tsx
@@ -0,0 +1,30 @@
+import { getServerAuthSession } from "@/server/auth";
+import { redirect } from "next/navigation";
+import Sidebar from "@/components/navigation/sidebar";
+import Header from "@/components/navigation/header";
+
+export default async function Dashboard({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await getServerAuthSession();
+
+  if (session) {
+    const hasUserName = session.user.name;
+
+    if (!hasUserName) {
+      redirect("/setup");
+    }
+
+    return (
+      <main className="flex">
+        <Sidebar />
+        <section className="w-full">
+          <Header session={session} />
+          {children}
+        </section>
+      </main>
+    );
+  }
+}
