ff56088647fdc68f40ce64b3a266e688ef8bdc1c
DiegoxK <diego.stv@outlook.es>
2024-06-12 18:05:25 -0500

    Update Dashboard page

--- src/app/(routes)/dashboard/page.tsx
+++ src/app/(routes)/dashboard/page.tsx
@@ -1,30 +1,11 @@
-import { getServerAuthSession } from "@/server/auth";
-import { redirect } from "next/navigation";
-import Image from "next/image";
-
-export default async function Dashboard() {
-  const session = await getServerAuthSession();
-
-  if (session) {
-    const hasUserName = session.user.name;
-
-    if (!hasUserName) {
-      redirect("/setup");
-    }
-  }
-
+export default function Content() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome, {session?.user.name}</p>
-      {session?.user.image && (
-        <Image
-          src={session.user.image}
-          alt="profile picture"
-          width={200}
-          height={200}
-        />
-      )}
-    </div>
+    <main className="flex h-5/6 items-center justify-center">
+      bacabailando
+      <img
+        src="https://media.tenor.com/9egJp0qwy_UAAAAi/polish-cow-polish.gif"
+        alt=""
+      />
+    </main>
   );
 }
