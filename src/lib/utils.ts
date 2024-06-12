369b0efbc1332868613fcc58f14b457a7e02af3a
DiegoxK <diego.stv@outlook.es>
2024-06-12 08:08:42 -0500

    Add cropImage function for image cropping functionality

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,50 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import type { RefObject } from "react";
+import type { Crop } from "react-image-crop";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const cropImage = async (
+  imgRef: RefObject<HTMLImageElement>,
+  crop: Crop,
+): Promise<Blob | undefined> => {
+  const image = imgRef.current;
+
+  if (!image || !crop) {
+    throw new Error("Crop canvas does not exist");
+  }
+
+  if (crop.width === 0 || crop.height === 0) {
+    return undefined;
+  }
+
+  const scaleX = image.naturalWidth / image.width;
+  const scaleY = image.naturalHeight / image.height;
+
+  const offscreen = new OffscreenCanvas(
+    crop.width * scaleX,
+    crop.height * scaleY,
+  );
+  const ctx = offscreen.getContext("2d");
+
+  if (!ctx) {
+    throw new Error("No 2d context");
+  }
+
+  ctx.drawImage(
+    image,
+    crop.x * scaleX,
+    crop.y * scaleY,
+    crop.width * scaleX,
+    crop.height * scaleY,
+    0,
+    0,
+    crop.width * scaleX,
+    crop.height * scaleY,
+  );
+
+  return await offscreen.convertToBlob();
+};
