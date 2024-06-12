8ca71348faa355cd010da669c817765197416f4e
DiegoxK <diego.stv@outlook.es>
2024-06-11 20:04:15 -0500

    Refactor userRouter and add protectedProcedure for hasUserName

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,8 +1,36 @@
-import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "@/server/api/trpc";
 import { users } from "@/server/db/schema";
 import { z } from "zod";
 
 export const userRouter = createTRPCRouter({
+  hasUserName: protectedProcedure
+    .input(
+      z.object({
+        email: z.string().email(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const userEmail = input.email;
+
+      const user = await ctx.db.query.users.findFirst({
+        where: (table, funcs) => funcs.eq(table.email, userEmail),
+      });
+
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      if (!user.name) {
+        return false;
+      }
+
+      return true;
+    }),
+
   createUser: publicProcedure
     .input(
       z.object({
