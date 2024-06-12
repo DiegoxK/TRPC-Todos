eb8bfd98b7e7fe32650ced58bff304fd241d2e5c
DiegoxK <diego.stv@outlook.es>
2024-06-12 13:27:30 -0500

    Added updateUser API endpoint for updating user information

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -4,6 +4,7 @@ import {
   publicProcedure,
 } from "@/server/api/trpc";
 import { users } from "@/server/db/schema";
+import { eq } from "drizzle-orm";
 import { z } from "zod";
 
 export const userRouter = createTRPCRouter({
@@ -51,5 +52,34 @@ export const userRouter = createTRPCRouter({
       return await ctx.db.insert(users).values({
         email: userEmail,
       });
+    }),
+
+  updateUser: protectedProcedure
+    .input(
+      z.object({
+        email: z.string().email(),
+        name: z.string(),
+        img: z.string().url().optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.session.user.id;
+      const userEmail = input.email;
+
+      const user = await ctx.db.query.users.findFirst({
+        where: (table, funcs) => funcs.eq(table.email, userEmail),
+      });
+
+      if (user && user.id === userId) {
+        return await ctx.db
+          .update(users)
+          .set({
+            name: input.name,
+            image: input.img,
+          })
+          .where(eq(users.id, userId));
+      }
+
+      throw new Error("Unauthorized");
     }),
 });
