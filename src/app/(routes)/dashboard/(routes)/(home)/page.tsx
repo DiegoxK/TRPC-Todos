472f29a0016f7f7d62b20d394c7f41f090be6362
DiegoxK <diego.stv@outlook.es>
2024-06-14 06:37:51 -0500

    Update Dashboard layout component structure and add ProjectCard component

--- src/app/(routes)/dashboard/(routes)/(home)/page.tsx
+++ src/app/(routes)/dashboard/(routes)/(home)/page.tsx
@@ -1,11 +1,34 @@
+import { Folder } from "lucide-react";
+
 export default function Content() {
   return (
-    <main className="flex h-5/6 items-center justify-center">
-      bacabailando
-      <img
-        src="https://media.tenor.com/9egJp0qwy_UAAAAi/polish-cow-polish.gif"
-        alt=""
-      />
-    </main>
+    <>
+      <h1 className="text-xl font-medium ">Projects</h1>
+      <p className="text-sm">A list of all your todos</p>
+      <div className="mt-4 grid grid-cols-4 gap-4">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+    </>
   );
 }
+
+const ProjectCard = () => {
+  return (
+    <div className="flex gap-2 rounded-lg bg-zinc-800 px-6 py-4 hover:outline hover:outline-2 hover:outline-primary">
+      <Folder className="mt-1" />
+      <div>
+        <h2 className="text-lg font-semibold">Portfolio</h2>
+        <p className="text-accent-background text-sm">Portfolio todos</p>
+      </div>
+    </div>
+  );
+};
