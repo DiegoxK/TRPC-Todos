f1354ac5d5c39710f373003bd93c8e43d6a744fe
DiegoxK <diego.stv@outlook.es>
2024-06-17 18:08:29 -0500

    Updated todoRouter to use protectedProcedure for getTodos

--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -1,25 +1,35 @@
-import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "@/server/api/trpc";
 import { todos } from "@/server/db/schema";
 import { eq } from "drizzle-orm";
 import { z } from "zod";
 
 export const todoRouter = createTRPCRouter({
-  getTodos: publicProcedure.query(({ ctx }) => {
-    return ctx.db.select().from(todos);
+  getTodos: protectedProcedure.query(({ ctx }) => {
+    const userId = ctx.session.user.id;
+
+    return ctx.db.query.todos.findMany({
+      where: eq(todos.createdById, userId),
+    });
   }),
-  setDone: publicProcedure
-    .input(
-      z.object({
-        id: z.string(),
-        done: z.boolean(),
-      }),
-    )
-    .mutation(async ({ ctx, input }) => {
-      return await ctx.db
-        .update(todos)
-        .set({
-          done: input.done,
-        })
-        .where(eq(todos.id, input.id));
-    }),
+
+  // return ctx.db.select().from(todos);
+  // setDone: publicProcedure
+  //   .input(
+  //     z.object({
+  //       id: z.string(),
+  //       done: z.boolean(),
+  //     }),
+  //   )
+  //   .mutation(async ({ ctx, input }) => {
+  //     return await ctx.db
+  //       .update(todos)
+  //       .set({
+  //         done: input.done,
+  //       })
+  //       .where(eq(todos.id, input.id));
+  //   }),
 });
