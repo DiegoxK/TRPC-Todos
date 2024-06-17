6282a47208c2a28afd7db48abe792d08ee3f7053
DiegoxK <diego.stv@outlook.es>
2024-06-17 18:10:58 -0500

    Moved ProjectCard component to its own file

---
+++ src/app/(routes)/dashboard/(routes)/(home)/_components/project-card.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import { Folder } from "lucide-react";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import Link from "next/link";
+
+export const ProjectCard = ({
+  projectId,
+  title,
+  description,
+}: {
+  projectId: string;
+  title: string;
+  description: string;
+}) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Link href={`/dashboard/project/${projectId}`}>
+            <Button
+              variant="ghost"
+              size="margin"
+              className="flex w-full items-start justify-start gap-2 rounded-lg border bg-background px-6 py-4 hover:outline hover:outline-2 hover:outline-offset-2 hover:outline-primary"
+            >
+              <Folder size={28} className="mt-1" />
+              <div className="text-start">
+                <h2 className="text-lg font-semibold text-zinc-300">{title}</h2>
+                <p className="text-sm text-zinc-500">
+                  {description.slice(0, 20)} ...
+                </p>
+              </div>
+            </Button>
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent side="bottom" sideOffset={10}>
+          <p>{description}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
--- src/app/(routes)/dashboard/(routes)/(home)/page.tsx
+++ src/app/(routes)/dashboard/(routes)/(home)/page.tsx
@@ -1,44 +1,32 @@
-import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { Folder } from "lucide-react";
-import Link from "next/link";
+import { api } from "@/trpc/server";
+import { ProjectCard } from "./_components/project-card";
 
-export default function Content() {
+export default async function Content() {
+  const projects = await api.project.getProjects();
+
   return (
     <>
       <h1 className="text-xl font-medium ">Projects</h1>
       <p className="text-sm">A list of your projects</p>
       <Separator className="mb-6 mt-5 bg-zinc-700" />
       <div className="mt-4 grid grid-cols-4 gap-4">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {[
+          ...projects,
+          ...projects,
+          ...projects,
+          ...projects,
+          ...projects,
+          ...projects,
+        ].map((project) => (
+          <ProjectCard
+            key={project.id}
+            projectId={project.id}
+            title={project.name}
+            description="Project description uwu"
+          />
+        ))}
       </div>
     </>
   );
 }
-
-const ProjectCard = () => {
-  return (
-    <Link href="/dashboard/projects/1">
-      <Button
-        variant="ghost"
-        size="margin"
-        className="flex w-full items-start justify-start gap-2 rounded-lg border bg-background px-6 py-4 hover:outline hover:outline-2 hover:outline-offset-2 hover:outline-primary"
-      >
-        <Folder size={28} className="mt-1" />
-        <div className="text-start">
-          <h2 className="text-lg font-semibold text-zinc-300">Portfolio</h2>
-          <p className="text-sm text-zinc-500">Portfolio todos</p>
-        </div>
-      </Button>
-    </Link>
-  );
-};
