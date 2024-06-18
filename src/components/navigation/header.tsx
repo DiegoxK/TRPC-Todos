b1a34bec214b0d97fe5fcba6f8161ba8b9f31e1f
DiegoxK <diego.stv@outlook.es>
2024-06-17 19:11:57 -0500

    Update header component to add breadcrumbs

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -6,18 +6,23 @@ interface HeaderProps {
 }
 
 export default function Header({ session }: HeaderProps) {
-  // TODO: Add breadcrumbs
   return (
-    <header className="fixed flex w-[calc(100%-20rem)] items-center justify-between bg-background px-7 py-4">
-      <h1 className="flex items-center gap-2 text-xl font-medium text-primary">
-        {/* Add button to retract the sidebar */}
-        <img
-          src="https://media.tenor.com/9egJp0qwy_UAAAAi/polish-cow-polish.gif"
-          width={36}
-        />
-        Bacavailando
-      </h1>
-      <UserButton session={session} />
-    </header>
+    <div className="fixed w-[calc(100%-20rem)] ">
+      <header className="flex items-center justify-between bg-background px-7 py-4">
+        <h1 className="flex items-center gap-2 text-xl font-medium text-primary">
+          {/* Add button to retract the sidebar */}
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            alt="Bacavailando"
+            src="https://media.tenor.com/9egJp0qwy_UAAAAi/polish-cow-polish.gif"
+            width={36}
+          />
+          Bacavailando
+        </h1>
+        <UserButton session={session} />
+      </header>
+      {/* TODO: Add breadcrumbs  here*/}
+      asdasd
+    </div>
   );
 }
