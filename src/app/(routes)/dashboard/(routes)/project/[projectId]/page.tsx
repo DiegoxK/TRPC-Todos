80f5b694b5efd253b1ffbda1aa9be68639dde48d
DiegoxK <diego.stv@outlook.es>
2024-06-17 19:02:30 -0500

    Added fetch project data

--- src/app/(routes)/dashboard/(routes)/project/[projectId]/page.tsx
+++ src/app/(routes)/dashboard/(routes)/project/[projectId]/page.tsx
@@ -1,3 +1,32 @@
-export default function ProjectPage() {
-  return <div>ProjectPage</div>;
+import { Separator } from "@/components/ui/separator";
+import { api } from "@/trpc/server";
+
+interface ProjectPageProps {
+  params: {
+    projectId: string;
+  };
+}
+
+export default async function ProjectPage({ params }: ProjectPageProps) {
+  const { projectId } = params;
+
+  const project = await api.project.getProject({
+    projectId,
+  });
+
+  return (
+    <>
+      <h1 className="text-xl font-medium ">{project?.name}</h1>
+      <p className="text-sm">Project Description</p>
+      <Separator className="mb-6 mt-5 bg-zinc-700" />
+      <div className="mt-4 grid grid-cols-4 gap-4">
+        {project?.todos.map((todo) => (
+          <div key={todo.id}>
+            <h2 className="text-lg font-medium">{todo.content}</h2>
+            {/* <p className="text-sm">{todo.description}</p> */}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
