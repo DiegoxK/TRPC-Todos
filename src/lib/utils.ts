7b5e7490a27d00f4866d6b593a7c2945692ca7e8
DiegoxK <diego.stv@outlook.es>
2024-06-19 10:09:05 -0500

    Add isUUID and generateSlug utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -48,3 +48,13 @@ export const cropImage = async (
 
   return await offscreen.convertToBlob();
 };
+
+export const isUUID = (uuid: string) => {
+  const UUID_REGEX =
+    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+  return UUID_REGEX.test(uuid);
+};
+
+function generateSlug(index: string) {
+  return encodeURIComponent(index.toLowerCase().replace(/\s+/g, "-"));
+}
