5e52a112daf2402a48ec50022570d19fdcc37ee7
DiegoxK <diego.stv@outlook.es>
2024-06-20 08:22:22 -0500

    Refactor project card component and add optional description field

--- src/app/(routes)/dashboard/(routes)/projects/_components/project-card.tsx
+++ src/app/(routes)/dashboard/(routes)/projects/_components/project-card.tsx
@@ -17,32 +17,51 @@ export const ProjectCard = ({
 }: {
   projectSlug: string;
   title: string;
-  description: string;
+  description?: string | null;
 }) => {
+  if (description) {
+    return (
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href={`/dashboard/projects/${projectSlug}`}>
+              <Button
+                variant="ghost"
+                size="margin"
+                className="flex w-full items-start justify-start gap-2 rounded-lg border bg-background px-6 py-4 hover:outline hover:outline-2 hover:outline-offset-2 hover:outline-primary"
+              >
+                <Folder size={28} className="mt-1" />
+                <div className="text-start">
+                  <h2 className="text-lg font-semibold text-zinc-300">
+                    {title}
+                  </h2>
+                  <p className="text-sm text-zinc-500">
+                    {description.slice(0, 20)} ...
+                  </p>
+                </div>
+              </Button>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side="bottom" sideOffset={10}>
+            <p>{description}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    );
+  }
+
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Link href={`/dashboard/projects/${projectSlug}`}>
-            <Button
-              variant="ghost"
-              size="margin"
-              className="flex w-full items-start justify-start gap-2 rounded-lg border bg-background px-6 py-4 hover:outline hover:outline-2 hover:outline-offset-2 hover:outline-primary"
-            >
-              <Folder size={28} className="mt-1" />
-              <div className="text-start">
-                <h2 className="text-lg font-semibold text-zinc-300">{title}</h2>
-                <p className="text-sm text-zinc-500">
-                  {description.slice(0, 20)} ...
-                </p>
-              </div>
-            </Button>
-          </Link>
-        </TooltipTrigger>
-        <TooltipContent side="bottom" sideOffset={10}>
-          <p>{description}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <Link href={`/dashboard/projects/${projectSlug}`}>
+      <Button
+        variant="ghost"
+        size="margin"
+        className="flex w-full items-center justify-start gap-2 rounded-lg border bg-background px-6 py-4 hover:outline hover:outline-2 hover:outline-offset-2 hover:outline-primary"
+      >
+        <Folder size={28} className="mt-1" />
+        <div className="text-start">
+          <h2 className="text-lg font-semibold text-zinc-300">{title}</h2>
+        </div>
+      </Button>
+    </Link>
   );
 };
