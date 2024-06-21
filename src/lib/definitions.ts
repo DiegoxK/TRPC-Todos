e96a23a95b0c2450f402767a8cec342c9ed095ac
DiegoxK <diego.stv@outlook.es>
2024-06-20 20:47:17 -0500

    Add CustomColumnDef type for table columns

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,5 +1,10 @@
+import type { ColumnDef } from "@tanstack/react-table";
 import { type todos } from "@/server/db/schema";
 
 // type user = typeof users.$inferSelect;
 
 export type Todo = typeof todos.$inferSelect;
+
+export type CustomColumnDef<TData, TValue> = ColumnDef<TData, TValue> & {
+  headClassName?: string;
+};
