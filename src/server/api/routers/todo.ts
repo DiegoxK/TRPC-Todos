7d90e713916da74b90ca6ebdaed0a89f1773959b
DiegoxK <diego.stv@outlook.es>
2024-06-24 15:14:20 -0500

    Added deleteTodos mutation to todoRouter

--- src/server/api/routers/todo.ts
+++ src/server/api/routers/todo.ts
@@ -1,10 +1,12 @@
+import type { TodoColumnName } from "@/lib/definitions";
 import {
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
 } from "@/server/api/trpc";
 import { todos } from "@/server/db/schema";
-import { eq } from "drizzle-orm";
+
+import { eq, getTableName } from "drizzle-orm";
 import { z } from "zod";
 
 export const todoRouter = createTRPCRouter({
@@ -16,7 +18,34 @@ export const todoRouter = createTRPCRouter({
     });
   }),
 
-  // return ctx.db.select().from(todos);
+  deleteTodos: protectedProcedure
+    .input(z.object({ ids: z.array(z.string()) }))
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.session.user.id;
+
+      const tableName = getTableName(todos);
+      const columnName: TodoColumnName = "createdById";
+      const whereClause = `(${input.ids.map((id) => `'${id}'`).join(", ")})`;
+
+      const query = `select * from "${tableName}" where "${columnName}" = '${userId}' and id in ${whereClause}`;
+
+      console.log("SQL_QUERY:", query);
+
+      // try {
+      //   return await ctx.db
+      //     .delete(todos)
+      //     .where(
+      //       sql`select * from todos where created_by_id = ${userId} and id in (${whereClause})`,
+      //     );
+      // } catch (err) {
+      //   console.error(err);
+      //   throw new TRPCError({
+      //     code: "INTERNAL_SERVER_ERROR",
+      //     message: "INTERNAL_SERVER_ERROR",
+      //   });
+      // }
+    }),
+
   // setDone: publicProcedure
   //   .input(
   //     z.object({
