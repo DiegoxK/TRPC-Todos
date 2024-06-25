15b3877a943777820e0120ebcd17b8ec2336144e
DiegoxK <diego.stv@outlook.es>
2024-06-25 16:53:46 -0500

    Remove unused imports and types in definitions.ts

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,4 +1,3 @@
-import type { ColumnDef } from "@tanstack/react-table";
 import { type todos } from "@/server/db/schema";
 
 // type user = typeof users.$inferSelect;
@@ -6,8 +5,3 @@ import { type todos } from "@/server/db/schema";
 export type Todo = typeof todos.$inferSelect;
 
 export type TodoColumnName = keyof Todo;
-
-export type CustomColumnDef<TData, TValue> = ColumnDef<TData, TValue> & {
-  headClassName?: string;
-  accessorKey?: string;
-};
