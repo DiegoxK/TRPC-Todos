9d42964022fdc32d95f86274a26617733c2f385b
DiegoxK <diego.stv@outlook.es>
2024-06-24 19:20:24 -0500

    Added Send button to CreateTaskForm component

--- src/app/(routes)/dashboard/(routes)/todos/_components/create-task-form.tsx
+++ src/app/(routes)/dashboard/(routes)/todos/_components/create-task-form.tsx
@@ -2,7 +2,7 @@
 
 import { Input } from "@/components/ui/input";
 import { TableCell, TableRow } from "@/components/ui/table";
-import { SquareMinus } from "lucide-react";
+import { Send, SquareMinus } from "lucide-react";
 
 interface CreateTaskFormProps {
   columnIds: (string | undefined)[];
@@ -10,25 +10,32 @@ interface CreateTaskFormProps {
 
 export default function CreateTaskForm({ columnIds }: CreateTaskFormProps) {
   return (
-    <TableRow>
+    <TableRow className="bg-accent">
       {columnIds.map((columnId, index) => {
         if (columnId === "select") {
           return (
             <TableCell className="pl-[15px] pr-0" key={index}>
               <SquareMinus
-                size={19}
-                className="cursor-pointer text-destructive hover:text-red-500"
+                size={20}
+                className="cursor-pointer text-accent-foreground transition-colors hover:text-destructive"
               />
             </TableCell>
           );
         }
         if (columnId === "actions") {
-          return <TableCell key={index}>a</TableCell>;
+          return (
+            <TableCell key={index}>
+              <Send
+                className="ml-2 cursor-pointer text-accent-foreground transition-colors hover:text-green-500"
+                size={19}
+              />
+            </TableCell>
+          );
         }
         return (
           <TableCell key={index}>
             <Input
-              className="h-8 w-52 border-border"
+              className="h-8 border-border"
               type="text"
               // placeholder={columnId}
             />
