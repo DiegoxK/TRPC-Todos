a54fdda8072db9d63f956e7af2631123de803148
DiegoxK <diego.stv@outlook.es>
2024-06-25 19:28:04 -0500

    Refactor Sidebar component

--- src/components/navigation/sidebar.tsx
+++ src/components/navigation/sidebar.tsx
@@ -2,7 +2,7 @@ import Navigation from "./navigation";
 
 export default function Sidebar() {
   return (
-    <section className="fixed hidden h-screen max-h-screen flex-col bg-background p-6 shadow-md md:flex md:w-[20rem]">
+    <section className="sticky top-0 hidden h-screen flex-col bg-background p-6 shadow-md md:flex md:min-w-[20rem]">
       <Navigation />
     </section>
   );
