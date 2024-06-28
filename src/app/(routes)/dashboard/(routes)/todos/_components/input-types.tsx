3917b8605779502d225c334db14075c3e77f99ea
DiegoxK <diego.stv@outlook.es>
2024-06-27 20:39:15 -0500

    Added InputCommand component for selecting status in Todos

--- src/app/(routes)/dashboard/(routes)/todos/_components/input-types.tsx
+++ src/app/(routes)/dashboard/(routes)/todos/_components/input-types.tsx
@@ -16,8 +16,17 @@ import { cn } from "@/lib/utils";
 import type { ControllerRenderProps } from "react-hook-form";
 
 import { format } from "date-fns";
-import { CalendarIcon } from "lucide-react";
+import { CalendarIcon, Check, ChevronsUpDown } from "lucide-react";
 import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 
 interface InputProps {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -60,6 +69,68 @@ export const InputDate = ({ field }: InputProps) => {
             disabled={(date) => date < new Date()}
             initialFocus
           />
+        </PopoverContent>
+      </Popover>
+      <FormMessage />
+    </FormItem>
+  );
+};
+
+export const InputCommand = ({
+  field,
+  values,
+}: InputProps & { values: string[] }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <FormItem className="flex flex-col">
+      <Popover modal={false} open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <FormControl>
+            <Button
+              variant="outline"
+              role="combobox"
+              className={cn(
+                " justify-between",
+                !field.value && "text-muted-foreground",
+              )}
+            >
+              {field.value
+                ? values.find((value) => value === field.value)
+                : "Select status"}
+              <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+            </Button>
+          </FormControl>
+        </PopoverTrigger>
+        <PopoverContent className="py-0 ps-0">
+          <Command>
+            <CommandInput placeholder="Search status..." />
+            <CommandList>
+              <CommandEmpty>No language found.</CommandEmpty>
+              <CommandGroup>
+                {values.map((value) => (
+                  <CommandItem
+                    value={value}
+                    key={value}
+                    onSelect={() => {
+                      setOpen(false);
+                      field.onChange(value);
+                      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+                      // form.setValue("typeDocBilling", value);
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        value === field.value ? "opacity-100" : "opacity-0",
+                      )}
+                    />
+                    {value}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
         </PopoverContent>
       </Popover>
       <FormMessage />
