9b1e57602c0495df9bdab955a403ea344f060eb7
DiegoxK <diego.stv@outlook.es>
2024-07-02 10:45:04 -0500

    Update InputCommand component to handle selecting status in Todos and CreateTaskForm

--- src/app/(routes)/dashboard/(routes)/todos/_components/create-task-form.tsx
+++ src/app/(routes)/dashboard/(routes)/todos/_components/create-task-form.tsx
@@ -83,8 +83,11 @@ export default function CreateTaskForm({
                 if (inputType === "date") {
                   return <InputDate field={field} />;
                 }
-                if (inputType instanceof Function) {
-                  return <InputCommand field={field} hook={inputType} />;
+                if (
+                  inputType instanceof Function ||
+                  inputType instanceof Array
+                ) {
+                  return <InputCommand field={field} values={inputType} />;
                 }
 
                 return <InputText field={field} />;
