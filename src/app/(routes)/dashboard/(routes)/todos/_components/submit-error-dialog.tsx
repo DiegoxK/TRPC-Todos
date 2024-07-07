59073ceda1f8543598fc15c8c3cfb974106f9b95
DiegoxK <diego.stv@outlook.es>
2024-07-07 10:49:17 -0500

    Refactor SubmitErrorDialog component to use Dialog component for error handling

--- src/app/(routes)/dashboard/(routes)/todos/_components/submit-error-dialog.tsx
+++ src/app/(routes)/dashboard/(routes)/todos/_components/submit-error-dialog.tsx
@@ -1,39 +1,43 @@
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-} from "@/components/ui/alert-dialog";
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { type Dispatch, type SetStateAction } from "react";
+import type { FieldErrors, FieldValues } from "react-hook-form";
 
-interface SubmitErrorDialogProps {
+interface SubmitErrorDialogProps<T extends FieldValues> {
   open: boolean;
+  errors?: FieldErrors<T>;
   onOpenChange: Dispatch<SetStateAction<boolean>>;
 }
 
-export default function SubmitErrorDialog({
+export default function SubmitErrorDialog<T extends FieldValues>({
   open,
+  errors,
   onOpenChange,
-}: SubmitErrorDialogProps) {
+}: SubmitErrorDialogProps<T>) {
+  if (!errors) return null;
+
   return (
-    <AlertDialog open={open} onOpenChange={onOpenChange}>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogTitle>Couldn&apos;t create task.</DialogTitle>
+        <DialogDescription className="space-y-2 rounded-md border border-red-400 bg-accent px-6 py-3">
+          {Object.entries(errors).map(([key, error]) => {
+            return (
+              <code className="text-red-400" key={key}>
+                {error?.message?.toString()}
+                <br />
+              </code>
+            );
+          })}
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
   );
 }
