038c3df045b93b0c02febdd296471a5e86f00851
DiegoxK <diego.stv@outlook.es>
2024-07-09 10:40:00 -0500

    Moved actions to its own component

---
+++ src/app/(routes)/dashboard/(routes)/todos/_components/table-actions.tsx
@@ -0,0 +1,121 @@
+import type { Todo } from "@/lib/definitions";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import type { z } from "zod";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { todoValidationSchema } from "./data";
+
+interface TableActionsProps {
+  todo: Todo;
+}
+
+export default function TableActions({ todo }: TableActionsProps) {
+  return (
+    <Dialog>
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button
+            className="absolute bottom-[18px] right-[18px] block h-4 w-4 rounded-none"
+            size="icon"
+            variant="ghost"
+          >
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal size={16} />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DialogTrigger asChild>
+            <DropdownMenuItem>Edit Task</DropdownMenuItem>
+          </DialogTrigger>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>View Task information</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DialogContent className="pl-6 pr-10">
+        <EditTodoForm todo={todo} />
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+const EditTodoForm = ({
+  todo: { id, task, projectId, description, due, priority, status },
+}: TableActionsProps) => {
+  const form = useForm<z.infer<typeof todoValidationSchema>>({
+    resolver: zodResolver(todoValidationSchema),
+    defaultValues: {
+      id,
+      task,
+      projectId,
+      description: description ?? "",
+      due,
+      priority,
+      status,
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof todoValidationSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="task"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
