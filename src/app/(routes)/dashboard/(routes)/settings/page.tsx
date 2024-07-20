dfaac9161fe4c56cfb6829f5ed20f6aab8cee595
DiegoxK <diego.stv@outlook.es>
2024-07-20 10:24:03 -0500

    Added profile form component for settings page

--- src/app/(routes)/dashboard/(routes)/settings/page.tsx
+++ src/app/(routes)/dashboard/(routes)/settings/page.tsx
@@ -1,3 +1,13 @@
+import { Separator } from "@/components/ui/separator";
+import ProfileForm from "./_components/profile-form";
+
 export default function SettingPage() {
-  return <div>SettingPage</div>;
+  return (
+    <>
+      <h1 className="text-xl font-medium ">Settings</h1>
+      <p className="text-sm">Profile configuration</p>
+      <Separator className="mb-6 mt-5 bg-zinc-700" />
+      <ProfileForm />
+    </>
+  );
 }
